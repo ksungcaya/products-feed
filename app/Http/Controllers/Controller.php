e4ebed6e724f05dffdc44ba5d17bfb859461f89e
Kenneth Sungcaya <ksungcaya@gmail.com>
2016-03-10 23:19:46 +0400

    add api reponse methods on base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,59 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var int
+     */
+    protected $statusCode = 200;
+    
+     /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param int $statusCode
+     *
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    /**
+     * Generic reponse
+     *
+     * @param $data
+     * @param array $headers
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function respond($data, $headers = [])
+    {
+        return response()->json($data, $this->getStatusCode(), $headers);
+    }
+
+    /**
+     * Generic error response with error message
+     *
+     * @param string $message
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message'     => $message,
+                'status_code' => $this->getStatusCode()
+            ]
+        ]);
+    }
 }
